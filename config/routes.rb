69c676db00cc7a95d24019e8c7ff84693883afdf
JMALL2 <janaymallory@gmail.com>
2023-04-10 13:54:46 +0000

    msm

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,27 @@
 Rails.application.routes.draw do
+  get("directors/youngest", { :controller => "director", :action => "newest"})
+
+
+  get("/directors/eldest", { :controller => "director", :action => "wisest" })
+ 
+ 
+  get("/directors/:an_id", { :controller => "director", :action => "director_details" })
+ 
+ 
   get("/", { :controller => "application", :action => "homepage" })
-end
+ 
+ 
+  get("/directors", { :controller => "director", :action => "index" })
+ 
+ 
+  get("/movies", { :controller => "movies", :action => "index" })
+ 
+ 
+  get("/movies/:an_id", { :controller => "movies", :action => "movie_details" })
+ 
+ 
+  get("/actors", { :controller => "actors", :action => "index" })
+ 
+ 
+  get("/actors/:an_id", { :controller => "actors", :action => "actor_details" })
+ end
